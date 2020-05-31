e876d5c5b7a2307f23041e3924640cb218d1b184
patrickf2000 <dev@patrickflynn.co>
2020-05-31 12:09:21 -0400

    added command line arguments

--- src/main.adb
+++ src/main.adb
@@ -1,14 +1,32 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Command_Line;
 
 with Ast_Tree; use Ast_Tree;
 with Parser; use Parser;
 with Debug; use Debug;
 
 procedure Main is
+    -- Rename clunky package
+    package ACL renames Ada.Command_Line;
+    
+    -- Local variables
     Ast : Tree;
+    File_Name : Unbounded_String := To_Unbounded_String("first.c");
+    
+    -- Parse command line arguments
+    procedure Parse_Args is
+        Index : Integer := 0;
+    begin
+        if ACL.Argument_Count > 0 then
+            File_Name := To_Unbounded_String(ACL.Argument(1));
+        end if;
+    end Parse_Args;
+    
 begin
-    Build_Tree(Ast, "first.c");
+    Parse_Args;
+    
+    Build_Tree(Ast, To_String(File_Name));
     Run_Pass2(Ast);
     Print_Tree(Ast);
 end Main;
---
+++ vars.c
@@ -0,0 +1,7 @@
+int main() {
+    int a;
+    int b = 1;
+    int c = b;
+    int d = 1 * 2 + c;
+    int e = a + b + c;
+}
