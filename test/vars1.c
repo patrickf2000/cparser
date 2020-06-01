705bf3981aa385b9b3bc0bab3c4cf7b53bf5efe5
patrickf2000 <dev@patrickflynn.co>
2020-06-01 10:39:26 -0400

    added variable test file

--- test/vars1.c
+++ test/vars1.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    int a;
-    a = 1;
-    int b;
-    b = a;
-    int c;
-    c = a + b;
-    int d;
-    d = a * b + 2;
-    int e;
-    e = 20 + 3 * a + b;
+    int a = 1;
+    int b = a;
+    int c = a + b;
+    int d = a * b + 2;
+    int e = 20 + 3 * a + b;
+    
     printf("%d\n", a);
     printf("%d\n", b);
     printf("%d\n", c);
     printf("%d\n", d);
     printf("%d\n", e);
+    
     a = b;
     b = 100;
     c = e;
     d = 1 + 2 + a + 3 + 4 + b;
-    e =  - 1 * a * b * c;
+    e = -1 * a * b * c;
+    
     printf("%d\n", a);
     printf("%d\n", b);
     printf("%d\n", c);
     printf("%d\n", d);
     printf("%d\n", e);
-    int x;
-    x = 2;
+
+    int x = 2;
     return 10 * x;
 }
-
