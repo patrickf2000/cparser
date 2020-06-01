e1a5586ec178ae17673d98dcc17bf581116afe94
patrickf2000 <dev@patrickflynn.co>
2020-06-01 11:55:50 -0400

    updated tests

--- test/test003.c
+++ test/test003.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 
 int add(int x, int y) {
-    int answer;
-    answer = x + y;
+    int answer = x + y;
     return answer;
 }
 
 void main() {
-    char v1;
-    v1 = 'a';
+    char v1 = 'a';
     short v2;
-    int v3;
-    v3 = add(2.00000E+00, 2.00000E+01);
-    float v4;
-    v4 = 1.23000E+00;
+    int v3 = add(2, 20);
+    float v4 = 1.23;
     double v5;
+    
     printf("%c\n", v1);
     printf("%d\n", v3);
     printf("%f\n", v4);
 }
-
